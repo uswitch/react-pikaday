b8422f2180cebcedb0ce997326bba4f793c4297a
Jack Stevens <jpstevens@users.noreply.github.com>
2017-06-14 11:42:42 +0100

    src -> lib

--- bundled.js
+++ bundled.js
@@ -1,8 +1,5 @@
-// XXX: this doesn't currently get compiled, so keep it es5!
-
-// this only works in webpack, but it's pretty rad:
 require('pikaday/css/pikaday.css');
 
-var ReactPikaday = require('./src/Pikaday');
+var ReactPikaday = require('./lib/Pikaday');
 
 module.exports = ReactPikaday;
